29ac657e8ac9949cee394da306c9544b3438040c
Alicja Dutkiewicz <ala@dutkiewicz.waw.pl>
2018-06-10 15:41:08 +0200

    (        krok= c(0),        proc_chorych= c(length(V(siec)[czy_chory==1])/vcount(siec)))

--- Siec/app.R
+++ Siec/app.R
@@ -1,6 +1,9 @@
 
 library(shiny)
 library(ggplot2)
+library(igraph)
+source('ewolucja.R')
+source('tworz_siec.R')
 
 ui <- fluidPage(
   
@@ -16,7 +19,7 @@ ui <- fluidPage(
                            "Pełny graf" = 'fg',
                            "Kwadratowa" = 'sq'), 
                          selected = 'ba'),
-             numericInput("N", label = "Liczba węzłów", value = 1e5),
+             numericInput("N", label = "Liczba węzłów", value = 100),
              sliderInput("beta",
                          label="Prawdopodobieństwo zarażenia",
                          min = 0, max = 1,
@@ -29,6 +32,7 @@ ui <- fluidPage(
                          label="Odsetek chorych na początku",
                          min = 0, max = 1,
                          value = 0.5, step = 0.01),
+             numericInput("l_krokow", label = "Liczba kroków", value = 100),
              actionButton("start", label = "Rozpocznij symulację")
            )
     ),
@@ -66,25 +70,50 @@ server <- function(input, output) {
          labs(colour="Liczba węzłów",shape="Typ sieci")+
        scale_color_continuous(limits=c(2,6),breaks=2:6,
                               labels=fancy_scientific(2:6))}
-     if(input$czy_N & !input$czy_rodzaj)
+     else if(input$czy_N & !input$czy_rodzaj)
      {g=g+geom_point(aes(x=wsp,y=procent_chorych,colour=log10(N)))+
        labs(colour="Liczba węzłów")+
        scale_color_continuous(limits=c(2,6),breaks=2:6,
                               labels=fancy_scientific(2:6))}
-     if(!input$czy_N & input$czy_rodzaj)
+     else if(!input$czy_N & input$czy_rodzaj)
      {g=g+geom_point(aes(x=wsp,y=procent_chorych,colour=siec_typ))+
        labs(colour="Typ sieci")}
-     if(!input$czy_N & !input$czy_rodzaj)
+     else if(!input$czy_N & !input$czy_rodzaj)
       {g=g+geom_point(aes(x=wsp,y=procent_chorych))}
     
-     g=g+stat_function(fun=Vectorize(p_sick))
+     g=g+stat_function(fun=Vectorize(p_sick))+
+       lims(y=c(0,1))
      g=g+labs(title='Diagram fazowy',
               x=expression(lambda/lambda["kr"]),
               y=expression("P(I,"*infinity*")"))
      g
    })
    output$symulacja <- renderPlot({
-     plot(1:4,1:4)
+     siec<-tworz_siec(N=input$N,proc_chorych=input$proc_chorych,typ=input$siec_typ)
+     
+     k_sr=mean(degree(siec,mode='all'))#sredni stopień węzła
+     lambda_kr=1/k_sr
+     lambda=input$beta/input$gamma
+      
+     #ramka z prawdopodobieństwem zachorowania od czasu
+     sym<-data.frame(
+       krok= c(0),
+       proc_chorych= c(length(V(siec)[czy_chory==1])/vcount(siec)))
+     
+     #ewolucja sieci
+     for(krok in 1:input$l_krokow){
+       siec=ewolucja(siec,beta=input$beta,gamma=input$gamma)
+       wyniki<-c(krok, length(V(siec)[czy_chory==1])/vcount(siec))
+       sym<-rbind(sym, wyniki)
+     }
+     #tworzenie wykresu P(I,t)
+     ggplot(sym)+geom_point(aes(x=krok,y=proc_chorych))+
+       lims(y=c(0,1))+
+       labs(y='P(I,t)',x='t',
+            title=bquote('<k>='*.(round(k_sr))
+                         ~lambda[KR]*'='*.(round(lambda_kr,2))
+                         ~lambda*'='*.(round(lambda,2))))+
+       geom_hline(yintercept = 1-lambda_kr/lambda)
    })
 }
 
